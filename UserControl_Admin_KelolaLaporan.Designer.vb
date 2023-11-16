ffe251df0200e2fd7b4f12de57aa27e664a7e4be
KeiAxiro <makfaruk6@gmail.com>
2023-11-16 14:33:38 +0700

    telah selesai membuat laporan transaksi

--- UserControl_Admin_KelolaLaporan.Designer.vb
+++ UserControl_Admin_KelolaLaporan.Designer.vb
@@ -22,9 +22,8 @@ Partial Class UserControl_Admin_KelolaLaporan
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -50,7 +49,7 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.Label1.Margin = New System.Windows.Forms.Padding(0)
         Me.Label1.Name = "Label1"
         Me.Label1.Padding = New System.Windows.Forms.Padding(3, 0, 0, 0)
-        Me.Label1.Size = New System.Drawing.Size(673, 40)
+        Me.Label1.Size = New System.Drawing.Size(667, 29)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Kelola Laporan"
         '
@@ -58,10 +57,10 @@ Partial Class UserControl_Admin_KelolaLaporan
         '
         Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
         Me.Label2.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(10, 50)
+        Me.Label2.Location = New System.Drawing.Point(10, 39)
         Me.Label2.Margin = New System.Windows.Forms.Padding(0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(673, 26)
+        Me.Label2.Size = New System.Drawing.Size(667, 26)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Laporan Penjualan"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -74,7 +73,7 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.Label3.Location = New System.Drawing.Point(0, 0)
         Me.Label3.Margin = New System.Windows.Forms.Padding(0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(134, 33)
+        Me.Label3.Size = New System.Drawing.Size(133, 33)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Dari Tgl."
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -84,10 +83,10 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.Label4.AutoSize = True
         Me.Label4.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Label4.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(268, 0)
+        Me.Label4.Location = New System.Drawing.Point(266, 0)
         Me.Label4.Margin = New System.Windows.Forms.Padding(0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(134, 33)
+        Me.Label4.Size = New System.Drawing.Size(133, 33)
         Me.Label4.TabIndex = 2
         Me.Label4.Text = "Sampai Tgl."
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -96,9 +95,9 @@ Partial Class UserControl_Admin_KelolaLaporan
         '
         Me.Button_KelolaLaporan_Load.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Button_KelolaLaporan_Load.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_KelolaLaporan_Load.Location = New System.Drawing.Point(539, 3)
+        Me.Button_KelolaLaporan_Load.Location = New System.Drawing.Point(535, 3)
         Me.Button_KelolaLaporan_Load.Name = "Button_KelolaLaporan_Load"
-        Me.Button_KelolaLaporan_Load.Size = New System.Drawing.Size(131, 27)
+        Me.Button_KelolaLaporan_Load.Size = New System.Drawing.Size(129, 27)
         Me.Button_KelolaLaporan_Load.TabIndex = 4
         Me.Button_KelolaLaporan_Load.Text = "Load"
         Me.Button_KelolaLaporan_Load.UseVisualStyleBackColor = True
@@ -117,11 +116,11 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.TableLayoutPanel_Admin_KelolaLaporan.Controls.Add(Me.DateTimePicker_KelolaLaporan_Dari, 1, 0)
         Me.TableLayoutPanel_Admin_KelolaLaporan.Controls.Add(Me.DateTimePicker_KelolaLaporan_Sampai, 3, 0)
         Me.TableLayoutPanel_Admin_KelolaLaporan.Dock = System.Windows.Forms.DockStyle.Top
-        Me.TableLayoutPanel_Admin_KelolaLaporan.Location = New System.Drawing.Point(10, 76)
+        Me.TableLayoutPanel_Admin_KelolaLaporan.Location = New System.Drawing.Point(10, 65)
         Me.TableLayoutPanel_Admin_KelolaLaporan.Name = "TableLayoutPanel_Admin_KelolaLaporan"
         Me.TableLayoutPanel_Admin_KelolaLaporan.RowCount = 1
         Me.TableLayoutPanel_Admin_KelolaLaporan.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel_Admin_KelolaLaporan.Size = New System.Drawing.Size(673, 33)
+        Me.TableLayoutPanel_Admin_KelolaLaporan.Size = New System.Drawing.Size(667, 33)
         Me.TableLayoutPanel_Admin_KelolaLaporan.TabIndex = 5
         '
         'DateTimePicker_KelolaLaporan_Dari
@@ -131,9 +130,9 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.DateTimePicker_KelolaLaporan_Dari.Dock = System.Windows.Forms.DockStyle.Top
         Me.DateTimePicker_KelolaLaporan_Dari.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DateTimePicker_KelolaLaporan_Dari.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.DateTimePicker_KelolaLaporan_Dari.Location = New System.Drawing.Point(137, 3)
+        Me.DateTimePicker_KelolaLaporan_Dari.Location = New System.Drawing.Point(136, 3)
         Me.DateTimePicker_KelolaLaporan_Dari.Name = "DateTimePicker_KelolaLaporan_Dari"
-        Me.DateTimePicker_KelolaLaporan_Dari.Size = New System.Drawing.Size(128, 26)
+        Me.DateTimePicker_KelolaLaporan_Dari.Size = New System.Drawing.Size(127, 26)
         Me.DateTimePicker_KelolaLaporan_Dari.TabIndex = 5
         '
         'DateTimePicker_KelolaLaporan_Sampai
@@ -143,19 +142,19 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.DateTimePicker_KelolaLaporan_Sampai.Dock = System.Windows.Forms.DockStyle.Fill
         Me.DateTimePicker_KelolaLaporan_Sampai.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DateTimePicker_KelolaLaporan_Sampai.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.DateTimePicker_KelolaLaporan_Sampai.Location = New System.Drawing.Point(405, 3)
+        Me.DateTimePicker_KelolaLaporan_Sampai.Location = New System.Drawing.Point(402, 3)
         Me.DateTimePicker_KelolaLaporan_Sampai.Name = "DateTimePicker_KelolaLaporan_Sampai"
-        Me.DateTimePicker_KelolaLaporan_Sampai.Size = New System.Drawing.Size(128, 26)
+        Me.DateTimePicker_KelolaLaporan_Sampai.Size = New System.Drawing.Size(127, 26)
         Me.DateTimePicker_KelolaLaporan_Sampai.TabIndex = 6
         '
         'DataGridView_Admin_KelolaLaporan
         '
         Me.DataGridView_Admin_KelolaLaporan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView_Admin_KelolaLaporan.Dock = System.Windows.Forms.DockStyle.Top
-        Me.DataGridView_Admin_KelolaLaporan.Location = New System.Drawing.Point(10, 109)
+        Me.DataGridView_Admin_KelolaLaporan.Location = New System.Drawing.Point(10, 98)
         Me.DataGridView_Admin_KelolaLaporan.Margin = New System.Windows.Forms.Padding(0)
         Me.DataGridView_Admin_KelolaLaporan.Name = "DataGridView_Admin_KelolaLaporan"
-        Me.DataGridView_Admin_KelolaLaporan.Size = New System.Drawing.Size(673, 162)
+        Me.DataGridView_Admin_KelolaLaporan.Size = New System.Drawing.Size(667, 141)
         Me.DataGridView_Admin_KelolaLaporan.TabIndex = 6
         '
         'Chart_Admin_kelolaLaporan
@@ -164,19 +163,18 @@ Partial Class UserControl_Admin_KelolaLaporan
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Chart_Admin_kelolaLaporan.BorderlineColor = System.Drawing.Color.Violet
-        ChartArea2.Name = "ChartArea1"
-        Me.Chart_Admin_kelolaLaporan.ChartAreas.Add(ChartArea2)
-        Legend2.Name = "Legend1"
-        Me.Chart_Admin_kelolaLaporan.Legends.Add(Legend2)
-        Me.Chart_Admin_kelolaLaporan.Location = New System.Drawing.Point(10, 277)
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart_Admin_kelolaLaporan.ChartAreas.Add(ChartArea1)
+        Me.Chart_Admin_kelolaLaporan.Location = New System.Drawing.Point(10, 242)
         Me.Chart_Admin_kelolaLaporan.Name = "Chart_Admin_kelolaLaporan"
         Me.Chart_Admin_kelolaLaporan.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None
         Me.Chart_Admin_kelolaLaporan.PaletteCustomColors = New System.Drawing.Color() {System.Drawing.Color.Navy, System.Drawing.Color.Aqua}
-        Series2.ChartArea = "ChartArea1"
-        Series2.Legend = "Legend1"
-        Series2.Name = "Series1"
-        Me.Chart_Admin_kelolaLaporan.Series.Add(Series2)
-        Me.Chart_Admin_kelolaLaporan.Size = New System.Drawing.Size(511, 160)
+        Series1.ChartArea = "ChartArea1"
+        Series1.Name = "Tgl_Transaksi_x_Total_Bayar"
+        Series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.[Date]
+        Series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.[Double]
+        Me.Chart_Admin_kelolaLaporan.Series.Add(Series1)
+        Me.Chart_Admin_kelolaLaporan.Size = New System.Drawing.Size(569, 200)
         Me.Chart_Admin_kelolaLaporan.TabIndex = 7
         Me.Chart_Admin_kelolaLaporan.Text = "Chart_Admin_KelolaLaporan"
         '
@@ -184,9 +182,9 @@ Partial Class UserControl_Admin_KelolaLaporan
         '
         Me.Button_KelolaLaporan_Generate.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button_KelolaLaporan_Generate.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_KelolaLaporan_Generate.Location = New System.Drawing.Point(527, 341)
+        Me.Button_KelolaLaporan_Generate.Location = New System.Drawing.Point(585, 341)
         Me.Button_KelolaLaporan_Generate.Name = "Button_KelolaLaporan_Generate"
-        Me.Button_KelolaLaporan_Generate.Size = New System.Drawing.Size(153, 27)
+        Me.Button_KelolaLaporan_Generate.Size = New System.Drawing.Size(89, 27)
         Me.Button_KelolaLaporan_Generate.TabIndex = 8
         Me.Button_KelolaLaporan_Generate.Text = "Generate"
         Me.Button_KelolaLaporan_Generate.UseVisualStyleBackColor = True
@@ -204,7 +202,7 @@ Partial Class UserControl_Admin_KelolaLaporan
         Me.Margin = New System.Windows.Forms.Padding(0)
         Me.Name = "UserControl_Admin_KelolaLaporan"
         Me.Padding = New System.Windows.Forms.Padding(10)
-        Me.Size = New System.Drawing.Size(693, 450)
+        Me.Size = New System.Drawing.Size(687, 450)
         Me.TableLayoutPanel_Admin_KelolaLaporan.ResumeLayout(False)
         Me.TableLayoutPanel_Admin_KelolaLaporan.PerformLayout()
         CType(Me.DataGridView_Admin_KelolaLaporan, System.ComponentModel.ISupportInitialize).EndInit()
--- UserControl_Admin_KelolaLaporan.vb
+++ UserControl_Admin_KelolaLaporan.vb
@@ -1,10 +1,14 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Windows.Forms.DataVisualization.Charting
 
 Public Class UserControl_Admin_KelolaLaporan
     Private Sub UserControl_Admin_KelolaLaporan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Call Kondisi_Awal()
     End Sub
-    Private Sub Show_Grid_Laporan()
+    Private Sub Kondisi_Awal()
+        Call Show_Grid_Laporan_All()
+    End Sub
+    Private Sub Show_Grid_Laporan_All()
         Call Koneksi()
         Dim GetDataLaporan As String = "SELECT Tgl_Transaksi,Total_Bayar FROM Tbl_Transaksi"
         Cmd = New SqlCommand(GetDataLaporan, Conn)
@@ -15,15 +19,55 @@ Public Class UserControl_Admin_KelolaLaporan
         DataGridView_Admin_KelolaLaporan.DataSource = Ds.Tables("Tbl_Transaksi")
     End Sub
 
-    Private Sub Kondisi_Awal()
-        Call Show_Grid_Laporan()
+    Private Sub Show_Grid_Laporan_Tgl()
+        Call Koneksi()
+        Dim GetDataLaporan As String = "SELECT Tgl_Transaksi,Total_Bayar FROM Tbl_Transaksi WHERE Tgl_Transaksi >= @P_DatePickerDari AND Tgl_Transaksi <= @P_DatePickerSampai ORDER BY Tgl_Transaksi"
+        Cmd = New SqlCommand(GetDataLaporan, Conn)
+        Cmd.Parameters.AddWithValue("@P_DatePickerDari", DateTimePicker_KelolaLaporan_Dari.Value.Date)
+        Cmd.Parameters.AddWithValue("@P_DatePickerSampai", DateTimePicker_KelolaLaporan_Sampai.Value.Date)
+        Sda = New SqlDataAdapter(Cmd)
+        Ds = New DataSet
+        Sda.Fill(Ds, "Tbl_Transaksi")
+
+        DataGridView_Admin_KelolaLaporan.DataSource = Ds.Tables("Tbl_Transaksi")
     End Sub
 
-    Private Sub Kondisi_Awal_Input()
+    Private Sub Generate_Chart_Laporan_Transaksi()
+        Call Koneksi()
+
+        Dim ChartKT As Chart = Chart_Admin_kelolaLaporan
+        ChartKT.Series.Clear()
+        Dim SeriesKT As New Series("Tgl_Transaksi_x_Total_Bayar")
+        Dim GetDataLaporanSum As String = "SELECT Tgl_Transaksi,SUM(Total_Bayar) AS Total_Bayar FROM Tbl_Transaksi WHERE Tgl_Transaksi >= @P_DatePickerDari AND Tgl_Transaksi <= @P_DatePickerSampai GROUP BY Tgl_Transaksi ORDER BY Tgl_Transaksi"
+
+        Cmd = New SqlCommand(GetDataLaporanSum, Conn)
+        Cmd.Parameters.AddWithValue("@P_DatePickerDari", DateTimePicker_KelolaLaporan_Dari.Value.Date)
+        Cmd.Parameters.AddWithValue("@P_DatePickerSampai", DateTimePicker_KelolaLaporan_Sampai.Value.Date)
+        Sda = New SqlDataAdapter(Cmd)
+        Ds = New DataSet
+        Sda.Fill(Ds)
+
+
+        For Each row As DataRow In Ds.Tables(0).Rows
+            Dim tgl_t As Date = CDate(row("Tgl_Transaksi"))
+            Dim total_bayar As Double = CDbl(row("Total_Bayar"))
+
+            SeriesKT.Points.AddXY(tgl_t, total_bayar)
+        Next
+
+        ChartKT.Series.Add(SeriesKT)
+        ChartKT.Series("Tgl_Transaksi_x_Total_Bayar").ChartType = SeriesChartType.Column
+        ChartKT.ChartAreas(0).AxisX.Title = "Tanggal"
+        ChartKT.ChartAreas(0).AxisY.Title = "Total Bayar"
 
     End Sub
 
     Private Sub Button_Load_Click(sender As Object, e As EventArgs) Handles Button_KelolaLaporan_Load.Click
-        Call Show_Grid_Laporan()
+        Call Show_Grid_Laporan_Tgl()
+
+    End Sub
+
+    Private Sub Button_KelolaLaporan_Generate_Click(sender As Object, e As EventArgs) Handles Button_KelolaLaporan_Generate.Click
+        Call Generate_Chart_Laporan_Transaksi()
     End Sub
 End Class
