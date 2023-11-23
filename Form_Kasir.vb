2be56fe1fa79ef89fba0e5dd04f5c6868c459712
KeiAxiro <makfaruk6@gmail.com>
2023-11-23 14:31:30 +0700

    srfsfr

--- Form_Kasir.vb
+++ Form_Kasir.vb
@@ -29,13 +29,44 @@ Public Class Form_Kasir
     End Sub
     Private Sub Panel_Input_Paint(sender As Object, e As PaintEventArgs) Handles Panel_NoResep.Paint, Panel_NamaPasien.Paint, Panel_NamaDokter.Paint, Panel_NamaObat.Paint, Panel_Harga.Paint, Panel_Quantity.Paint
         Dim Pen As New Pen(Color.FromArgb(165, 0, 0, 0), 2)
+        Dim TextBoxSize As Point = TextBox_NoResep.Size
+        Dim PanelSize As Point = Panel_NoResep.Size
+        Dim TextBoxLocation As Point = TextBox_NoResep.Location
+        Dim XPos As Integer = PanelSize.X - TextBoxSize.X
+
+        Dim StartP As Point = New Point(XPos, TextBox_NoResep.Bottom)
+        Dim EndP As Point = New Point(PanelSize.X, TextBox_NoResep.Bottom)
         Using Pen
-            e.Graphics.DrawLine(Pen, New Point(), New Point())
+            e.Graphics.DrawLine(Pen, StartP, EndP)
         End Using
     End Sub
 
     Private Sub Kondisi_Awal()
         Label_Nama_Kasir.Text = Form_Login.TextBox_Login_Username.Text.ToString
+        Clear_Input()
+    End Sub
+
+    Private Sub TextBox_Number(sender As Object, e As KeyPressEventArgs) Handles TextBox_Harga.KeyPress, TextBox_Quantity.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Clear_Input()
+        ComboBox_TypeResep.ResetText()
+        TextBox_NoResep.Clear()
+        DateTimePicker_Tgl_Resep.ResetText()
+        TextBox_NamaPasien.Clear()
+        TextBox_NamaDokter.Clear()
+        TextBox_NamaObat.Clear()
+        TextBox_Harga.Clear()
+        TextBox_Quantity.Clear()
+    End Sub
+
+    Private Sub Kondisi_Input_Resep(kondisi As Boolean)
+
+    End Sub
+    Private Sub Kondisi_Input_NonResep(kondisi As Boolean)
 
     End Sub
 
